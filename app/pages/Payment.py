81d655364a5f085ac450143361f6e09bf359a281
jeffry <jeffry@xvm.ch>
2026-03-09 05:26:12 +0200

    Ready for git

--- app/pages/Payment.py
+++ app/pages/Payment.py
@@ -12,7 +12,7 @@ st.set_page_config(
     layout="wide"
 )
 
-df = pd.read_csv('data\cleaned_data\cleaned_data.csv')
+df = pd.read_csv('data/cleaned_data/cleaned_data.csv')
 
 
 # 1. CSS: Erzwingt 0px oben und 50px unten
